2f2f9435e25211fd0e6903d89dbd681a61df5ab1
kritikabang <kbang443@gmail.com>
2017-10-15 22:33:28 +0530

    First

---
+++ app/views/includes/head.blade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kritika Bang
+ * Date: 15-10-2017
+ * Time: 22:07
+ */
+
+
+
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Scotch">
+
+<title>Super Cool Layouts</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+
---
+++ app/views/pages/blog.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kritika Bang
+ * Date: 15-10-2017
+ * Time: 22:09
+ */
